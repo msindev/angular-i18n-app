8727df62f9d720d917a5b947c11778bf8e0d95e5
mohitsi2 <mohitsi2@cisco.com>
2022-11-20 21:55:01 +0530

    Add Language Selector in Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  availableLanguages = {
-    English: $localize`:English Language Dropdown: English`,
-    Hindi: $localize`:Hindi Language Dropdown:Hindi`,
-  };
-  availableLanguagesDropdown = Object.values(this.availableLanguages);
-  selectedLanguage = this.availableLanguages.English;
+export class NavbarComponent implements OnInit {
+  availableLanguages = [
+    { code: 'en-US', name: 'English' },
+    { code: 'hi', name: 'हिन्दी' },
+  ];
+
+  selectedLanguage = 'English';
+
+  constructor(@Inject(LOCALE_ID) private readonly localeId: string) {}
+  ngOnInit(): void {
+    this.selectedLanguage =
+      this.availableLanguages.find(
+        (localeObj) => localeObj.code === this.localeId
+      )?.name ?? '';
+  }
+
+  changeLocale(locale: string): void {
+    window.location.href = window.location.origin + '/' + locale;
+  }
 }
